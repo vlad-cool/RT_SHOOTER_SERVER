3f324ebd2a5fd1e396632240162ddffcf5bb6092
vlad-cool <vlad-matveev2004@mail.ru>
2025-04-01 14:05:16 +0300

    added scripts and images sending

--- js/motion_control.js
+++ js/motion_control.js
@@ -1,62 +1,28 @@
-// let accelerometer = new Accelerometer({ frequency: 60 });
-// // accelerometer.addEventListener('reading', () => {
-// //     console.log(`Acceleration: X=${accelerometer.x}, Y=${accelerometer.y}, Z=${accelerometer.z}`);
-// // });
-// // accelerometer.start();
-
-// let gyroscope = new Gyroscope({ frequency: 60 });
-// // gyroscope.addEventListener('reading', () => {
-// //     console.log(`Rotation: X=${gyroscope.x}, Y=${gyroscope.y}, Z=${gyroscope.z}`);
-// // });
-// // gyroscope.start();
-
 var acceleration = undefined;
 var direction = undefined;
 var direction_vector = [0, 0, 1];
 
+var tap = false;
+var hold = false;
+
 window.addEventListener('devicemotion', (event) => {
     acceleration = event.accelerationIncludingGravity;
-
-    // acceleration.x += event.acceleration.x;
-    // acceleration.y += event.acceleration.y;
-    // acceleration.z += event.acceleration.z;
-
-    // console.log('Acceleration:', event.acceleration);
-    // console.log('Acceleration (incl. gravity):', event.accelerationIncludingGravity);
-    // console.log('Rotation rate:', event.rotationRate);
 });
 
 window.addEventListener('deviceorientationabsolute', (event) => {
     direction = event;
-
-    // function eulerToVector(eulerAngles, initialVector = [0, 0, 1]) {
-    const [yaw, pitch, roll] = [-direction.alpha * Math.PI / 180, direction.beta * Math.PI / 180, direction.gamma * Math.PI / 180];
-    const [x, y, z] = [0, -1, 0];
-
-    const cosYaw = Math.cos(yaw);
-    const sinYaw = Math.sin(yaw);
-    const x1 = x * cosYaw - y * sinYaw;
-    const y1 = x * sinYaw + y * cosYaw;
-    const z1 = z;
-
-    const cosPitch = Math.cos(pitch);
-    const sinPitch = Math.sin(pitch);
-    const x2 = x1 * cosPitch + z1 * sinPitch;
-    const y2 = y1;
-    const z2 = -x1 * sinPitch + z1 * cosPitch;
-
-    const cosRoll = Math.cos(roll);
-    const sinRoll = Math.sin(roll);
-    const x3 = x2;
-    const y3 = y2 * cosRoll - z2 * sinRoll;
-    const z3 = y2 * sinRoll + z2 * cosRoll;
-
-    direction_vector = [x3, y3, z3];
 });
 
 function read_data() {
-    console.log(`Acceleration: X=${acceleration.x.toFixed(8).padStart(12, ' ')}, Y=${acceleration.y.toFixed(8).padStart(12, ' ')}, Z=${acceleration.z.toFixed(8).padStart(12, ' ')}, alpha=${direction.alpha.toFixed(8).padStart(12, ' ')}, beta=${direction.beta.toFixed(8).padStart(12, ' ')}, gamma=${direction.gamma.toFixed(8).padStart(12, ' ')}`);
-    console.log(`Direction vector: X=${direction_vector[0].toFixed(8).padStart(12, ' ')}, Y=${direction_vector[1].toFixed(8).padStart(12, ' ')}, Z=${direction_vector[2].toFixed(8).padStart(12, ' ')}`);
+    console.log("AAA");
+    var alpha = direction.alpha * Math.PI / 180;
+
+    var beta = Math.acos(acceleration.y / Math.sqrt(acceleration.x ** 2 + acceleration.y ** 2 + acceleration.z ** 2));
+
+    direction_vector = [Math.cos(alpha) * Math.sin(beta), Math.sin(alpha) * Math.sin(beta), Math.cos(beta)];
+
+    // console.log(`Acceleration: X=${acceleration.x.toFixed(8).padStart(12, ' ')}, Y=${acceleration.y.toFixed(8).padStart(12, ' ')}, Z=${acceleration.z.toFixed(8).padStart(12, ' ')}, alpha=${direction.alpha.toFixed(8).padStart(12, ' ')}, beta=${direction.beta.toFixed(8).padStart(12, ' ')}, gamma=${direction.gamma.toFixed(8).padStart(12, ' ')}`);
+    // console.log(`Direction vector: X=${direction_vector[0].toFixed(8).padStart(12, ' ')}, Y=${direction_vector[1].toFixed(8).padStart(12, ' ')}, Z=${direction_vector[2].toFixed(8).padStart(12, ' ')}`);
     // console.log(`Rotation: X=${gyroscope.x}, Y=${gyroscope.y}, Z=${gyroscope.z}`);
 
     const xhr = new XMLHttpRequest();
@@ -65,7 +31,6 @@ function read_data() {
 
     xhr.onload = function () {
         if (xhr.status >= 200 && xhr.status < 300) {
-            console.log('Success:', JSON.parse(xhr.responseText));
         } else {
             console.error('Error:', xhr.statusText);
         }
@@ -73,7 +38,19 @@ function read_data() {
     xhr.onerror = function () {
         console.error('Request failed');
     };
-    xhr.send(JSON.stringify({ x: direction_vector[0], y: direction_vector[1], z: direction_vector[2] }));
+    xhr.send(JSON.stringify({ x: direction_vector[0], y: direction_vector[1], z: direction_vector[2], tap: hold, hold: !tap && hold }));
+    tap = false;
 }
+
+document.addEventListener('touchstart', function (e) {
+    console.log('Screen tapped at:', e.touches[0].clientX, e.touches[0].clientY);
+    tap = true;
+    hold = true;
+});
+
+document.addEventListener('touchend', function (e) {
+    console.log('Screen tapped at:', e.touches[0].clientX, e.touches[0].clientY);
+    hold = false;
+});
 
 setInterval(read_data, 200);
